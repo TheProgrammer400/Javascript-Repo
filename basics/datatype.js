15aa3083a304631b2d89281814fcbed926a660ec
Shrey Tank <85757750+TheProgrammer400@users.noreply.github.com>
2025-02-16 11:01:51 +0000

    datatypes and v ars

---
+++ basics/datatype.js
@@ -0,0 +1,12 @@
+let name = 'unknown' // string
+let age = 23 //  integer
+let eligible = true // boolean
+let address = 'world' // can be in '' too
+
+console.table([name, age, eligible, address])
+
+// null --> standalone value
+// symbol --> used for uniqueness
+
+console.log(typeof(name))
+console.log(typeof(address))
---
+++ basics/vars.js
@@ -0,0 +1,9 @@
+const n1 = 23; // fixing the value
+let n2 = 1212; // value can be changed
+var n3 = "3434"; // not to be used as causes issue in blocks
+n4 = "Hello world"; // also, not be used
+
+// n1 = "bye world"; // will throw error
+n2 = "1122"
+
+console.table([n1, n2, n3, n4]);
