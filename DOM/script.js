47557c422c73157361ba81bc26badcf5df4b6195
The Programmer <shrey.b.tank@gmail.com>
2025-02-24 22:36:20 +0530

    basic DOM

---
+++ DOM/script.js
@@ -0,0 +1,16 @@
+const t1 = document.getElementById("title") // select heading through id
+const h1 = document.getElementsByClassName("heading") // select heading through class name
+
+console.log(t1.getAttribute("id"))
+console.log(h1[0].getAttribute("class"))
+
+/*
+Note:
+    getElementsByClassName always return an collection. That's 
+    why while using .getAttribute(), we have to write h1[0] to 
+    access the first heading and same goes for .setAttribute() method
+*/
+
+// h1[0].setAttribute("class", "test")
+console.log(h1)
+// console.log(h1[0].getAttribute("class"))
